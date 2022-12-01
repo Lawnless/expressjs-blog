50267e9f39f5c984d1aea4c9a0a680618386bcbc
Lawnless <69431018+Lawnless@users.noreply.github.com>
2022-12-01 16:30:45 +0300

    Bootstrap theme update

--- index.js
+++ index.js
@@ -3,24 +3,19 @@ const app = express();
 const path = require('path');
 const port = 3000;
 const { success, info, warning, error } = require('./src/functions/messages.js');
-// const mysqlDB = require('./src/functions/connection.js');
-
-// mysqlDB.getConnection((err, connection) => {
-// 	mysqlDB.query('SELECT * FROM blog_posts');
-
-// });
 
 var blog_posts = [
-	{ postId: 0, postAuthor: 'Lawnless', postText: 'Hello World!', postDate: '30/11/2022 13:57' },
-	{ postId: 1, postAuthor: 'Isa Haktan Colak', postText: 'Hello World 2!', postDate: '30/11/2022 13:57' },
-	{ postId: 2, postAuthor: 'Yesa Altun', postText: 'Hello World 3!', postDate: '30/11/2022 13:57' }
+	{ postId: 0, postAuthor: 'John Doe', postTitle: 'This is title 1!', postText: 'Hello World!', postDate: '30/11/2022 13:57' },
+	{ postId: 1, postAuthor: 'Mark Doe', postTitle: 'And it is 2!', postText: 'Hello Earth!', postDate: '30/11/2022 13:57' },
+	{ postId: 2, postAuthor: 'Michael Doe', postTitle: 'This one is 3!', postText: 'Hello Mars!', postDate: '30/11/2022 13:57' }
 ];
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './src/views'));
+app.use('/static', express.static(path.join(__dirname, './src')));
 
 app.get('/', (req, res) => {
-	res.render('index');
+	res.render('index', { blog_posts: blog_posts });
 });
 
 app.get('/about', (req, res) => {
@@ -36,6 +31,7 @@ app.get('/read/:postId', (req, res) => {
 		res.render('single-post', {
 			postId: req.params.postId,
 			postAuthor: blog_posts[req.params.postId].postAuthor,
+			postTitle: blog_posts[req.params.postId].postTitle,
 			postText: blog_posts[req.params.postId].postText,
 			postDate: blog_posts[req.params.postId].postDate
 		});
